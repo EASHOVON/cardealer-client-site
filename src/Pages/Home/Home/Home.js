4f18319692ff3fe646b7c463327d047e3bebdfed
EASHOVON <ashrafuzzaman.shovon.786@gmail.com>
2021-11-12 06:57:35 +0600

    Navigation Designed

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navigation from '../Shared/Navigation/Navigation';
+
+const Home = () =>
+{
+    return (
+        <div>
+            <Navigation></Navigation>
+            <h2>HomePage</h2>
+        </div>
+    );
+};
+
+export default Home;
